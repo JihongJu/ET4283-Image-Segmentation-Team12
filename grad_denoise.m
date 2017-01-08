08f763eef61cfabe78943af35dcd519aa3f0c71f
Jihong Ju <daniel.jihong.ju@gmail.com>
2017-01-08 21:38:07 +0100

    update grad denoise

--- grad_denoise.m
+++ grad_denoise.m
@@ -5,12 +5,21 @@ else
     BW = imin;
 end
 
-% fill small holes
-filled = imfill(BW, 'holes');
-holes = filled & ~BW;
-bigholes = bwareaopen(holes, width);
-smallholes = holes & ~bigholes;
-BW(smallholes)=0;
+% % fill small holes
+% filled = imfill(BW, 'holes');
+% holes = filled & ~BW;
+% bigholes = bwareaopen(holes, width);
+% smallholes = holes & ~bigholes;
+% figure(3);
+% imshow(smallholes);
+% % BW = BW | smallholes;
+% BW(smallholes)=1;
+
+% fill
+BW = ~bwareaopen(~BW, width);
+% % erode
+% se = strel('disk',1);
+% BW = imerode(BW, se);
 
 imout = BW;
 end
